4e3ce3dc7f1a3b7e49503370166f870455ff937c
IkehAkinyemi <mrikehchukwuka@gmail.com>
2021-11-09 14:14:59 +0100

    Created script files for creatNewGame and joinGame functions

---
+++ scripts/create_new_game.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# export function createdNewGame(): GameID
+near call $CONTRACT createNewGame --account_id $SIGNER --amount 0.5
+
+exit 0
---
+++ scripts/join_game.sh
@@ -0,0 +1,4 @@
+# export function createdNewGame(): GameID
+near call $CONTRACT joinGame --account_id $PLAYER '{"gameId": "BD-4124384965"}' --amount 0.5
+
+exit 0
