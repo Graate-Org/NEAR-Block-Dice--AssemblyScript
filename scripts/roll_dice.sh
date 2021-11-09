26ec9b2834ed37af13a3f7c94bfdedd3d170078e
melvin_manni <melvinnnamani@gmail.com>
2021-11-09 15:03:44 +0100

    adjustment on scripts

--- scripts/roll_dice.sh
+++ scripts/roll_dice.sh
@@ -1,4 +1,4 @@
-# export function createdNewGame(): GameID
-near call $CONTRACT rollDice --account_id $PLAYER '{"gameId": "BD-4124384965"}'
+# export function rollDIce(): GameID
+near call $CONTRACT rollDice --account_id $PLAYER '{"gameId": "BD-412484965"}'
 
 exit 0
