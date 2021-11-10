343227f594c0c0ec4d19df75c4633121289be19b
melvin_manni <melvinnnamani@gmail.com>
2021-11-10 13:04:50 +0100

    ++ get_game_details, roll_dice

---
+++ scripts/get_game_details.sh
@@ -0,0 +1,5 @@
+
+# export function getGameDetails(): GameID
+near view $CONTRACT getGameDetails '{"gameId": "BD-4124384965"}'
+
+exit 0
--- scripts/roll_dice.sh
+++ scripts/roll_dice.sh
@@ -1,4 +1,4 @@
 # export function rollDIce(): GameID
-near call $CONTRACT rollDice --account_id $PLAYER '{"gameId": "BD-412484965"}'
+near call $CONTRACT rollDice --account_id $PLAYER '{"gameId": "BD-4124384965"}'
 
 exit 0
